e96de664c93c7e38c3a13a0ebb16e62ff4f4c591
bzamanbd <riponzamanbd@gmail.com>
2022-09-02 12:05:22 +0600

    init

--- get_show_dialog/lib/app/themes/text.dart
+++ get_show_dialog/lib/app/themes/text.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
 import '../app_con.dart';
 
 class Text {
   TextTheme appText = TextTheme(
     bodyText1: TextStyle(
       color: AppCon.defaultColorTheme.textColor,
-      fontSize: Get.size.width * 0.06,
+      fontSize: AppCon.defaultSize,
       fontWeight: FontWeight.normal,
     ),
     bodyText2: TextStyle(
       color: AppCon.defaultColorTheme.textColor,
-      fontSize: Get.size.width * 0.06,
+      fontSize: AppCon.defaultSize,
       fontWeight: FontWeight.normal,
     ),
   );

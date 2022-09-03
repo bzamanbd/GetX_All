a0c6e33e2d401bd48786decce951f6b62307bdd5
bzamanbd <riponzamanbd@gmail.com>
2022-09-03 12:36:52 +0600

    done

--- getx_bottom_sheet_and_dynamic_theme/lib/app/modules/home/views/home_view.dart
+++ getx_bottom_sheet_and_dynamic_theme/lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:getx_bottom_sheet_and_dynamic_theme/app/app_con.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -8,16 +9,32 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: Text('HomeView'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Center(
+          child: AppCon.commonWidgets.defaultBtn(
+            btnText: 'BottomSheet',
+            onTap: () => Get.bottomSheet(
+              Container(
+                child: Wrap(
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.wb_sunny_outlined),
+                      title: Text('Day'),
+                      onTap: () => Get.changeTheme(ThemeData.light()),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.wb_sunny),
+                      title: Text('Dark'),
+                      onTap: () => Get.changeTheme(ThemeData.dark()),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }

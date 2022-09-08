32d0a9d3bbc73f8bbdc661b3faaa743e80cae009
bzamanbd <riponzamanbd@gmail.com>
2022-09-08 17:43:43 +0600

    init

--- getx_internationalization/lib/main.dart
+++ getx_internationalization/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:getx_internationalization/app/languages/messages.dart';
-
+import 'package:getx_internationalization/app_con.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
+      theme: ThemeData(
+        primarySwatch: AppCon.defaultColorTheme.swColor,
+      ),
       translations: Messags(),
       locale: Get.deviceLocale,
       fallbackLocale: Locale('en', 'US'),
